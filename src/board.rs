ce9032f52412c8e9d2ba834184a4adc331f2eb63
Benjamin Falk <benjamin.falk@yahoo.com>
2021-09-09 16:58:01 -0400

    A better understanding of powers

--- src/board.rs
+++ src/board.rs
@@ -9,30 +9,6 @@ pub struct Board {
     tiles: Vec<Tile>,
 }
 
-#[derive(Copy, Clone)]
-pub struct Coordinate {
-    pub(crate) x: usize,
-    pub(crate) y: usize,
-}
-
-impl Coordinate {
-    #[inline]
-    fn index_for_board(&self) -> usize {
-        self.x + (self.y * self.x)
-    }
-
-    #[inline]
-    fn validate_against_board(&self, board: &Board) {
-        if self.x >= board.x_length {
-            panic!("{} exceeds x_length {}", self.x, board.x_length);
-        }
-
-        if self.y >= board.y_length {
-            panic!("{} exceeds y_length {}", self.y, board.y_length);
-        }
-    }
-}
-
 impl Board {
     ///
     /// Create a new board of a given x and y size
@@ -57,45 +33,17 @@ impl Board {
         }
     }
 
-    #[inline]
-    /// The x length, or also, how far to the "right"
-    pub fn x_length(&self) -> usize {
-        self.x_length
-    }
-
-    #[inline]
-    /// The y length, or also, how far "down"
-    pub fn y_length(&self) -> usize {
-        self.y_length
-    }
-
     ///
-    /// Get tile reference given an x and y coordinate.
-    /// Note that the coordinates start at 0,0.
+    /// A read-only reference to the tile at the given index
     ///
-    /// panics if x or y goes beyond the board bounds
-    ///
-    pub fn tile_at(&self, coord: &Coordinate) -> &Tile {
-        coord.validate_against_board(self);
-        &self.tiles[coord.index_for_board()]
-    }
-
-    ///
-    /// Get mutable tile reference given an x and y coordinate.
-    /// Note that the coordinates start at 0,0.
-    ///
-    /// panics if x or y goes beyond the board bounds
-    ///
-    pub fn tile_at_mut(&mut self, coord: &Coordinate) -> &mut Tile {
-        coord.validate_against_board(self);
-        &mut self.tiles[coord.index_for_board()]
-    }
-
     pub fn tile_at_index(&self, index: usize) -> &Tile {
         &self.tiles[index]
     }
 
-    pub fn tile_at_index_mut(&mut self, index: usize) -> &Tile {
+    ///
+    /// A mutable reference to the tile at the given index
+    ///
+    pub fn tile_at_index_mut(&mut self, index: usize) -> &mut Tile {
         &mut self.tiles[index]
     }
 }
--- src/index.rs
+++ src/index.rs
@@ -4,7 +4,7 @@
 use bevy::math::Vec3;
 use bevy::ecs::entity::Entity;
 use std::collections::HashMap;
-use crate::board::{Board, Coordinate};
+use crate::board::Board;
 
 
 type EntityLookup = HashMap<Entity, EntityIndex>;
@@ -19,18 +19,32 @@ pub struct Index {
     board: Board,
     x_units_width: usize,
     y_units_width: usize,
+    cell_shift: usize,
+    y_shift: usize,
 }
 
 impl Index {
-    pub fn one_hundred_square_ten_unit_titles() -> Self {
+    pub fn square(unit_size: usize, subsection_size: usize) -> Self {
+        let cell_shift = (
+            (((unit_size / subsection_size) as f32).ln() / (2.0 as f32).ln()).round()
+        ) as usize;
+
+        let y_shift = (
+            ((subsection_size as f32).ln() / (2.0 as f32).ln()).round()
+        ) as usize;
+
+
         Self {
             entity_lookup: EntityLookup::with_capacity(200),
-            board: Board::new(10, 10),
-            x_units_width: 100,
-            y_units_width: 100,
+            board: Board::new(subsection_size, subsection_size),
+            x_units_width: unit_size,
+            y_units_width: unit_size,
+            cell_shift,
+            y_shift,
         }
     }
 
+    #[allow(unused_variables)]
     pub fn add_or_update_entity(&mut self, entity: &Entity, position: &Vec3) {
         let Self { ref mut entity_lookup, ref mut board, .. } = self;
 
@@ -40,5 +54,58 @@ impl Index {
             Some(entity_index) => {
             }
         }
+    }
+
+    fn board_index_for_vector(&self, position: &Vec3) -> usize {
+        let y = position.y as usize >> self.cell_shift;
+        let x = position.x as usize >> self.cell_shift;
+        x + (y << self.y_shift)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy::math::Vec3;
+    use super::*;
+
+    #[test]
+    fn the_index_system_works() {
+        let index = Index::square(4096, 64);
+        let near_zero = Vec3::new(1.0, 1.0, 0.0);
+        let near_sixtyfour = Vec3::new(63.9, 63.9, 0.0);
+        let over_one = Vec3::new(64.0, 18.0, 0.0);
+        let down_one = Vec3::new(18.0, 64.0, 0.0);
+        let over_one_down_one = Vec3::new(64.0, 64.0, 0.0);
+        let end_of_world = Vec3::new(4095.9, 4095.9, 0.0);
+
+        assert_eq!(
+            index.board_index_for_vector(&near_zero),
+            0
+        );
+
+        assert_eq!(
+            index.board_index_for_vector(&near_sixtyfour),
+            0
+        );
+
+        assert_eq!(
+            index.board_index_for_vector(&over_one),
+            1
+        );
+
+        assert_eq!(
+            index.board_index_for_vector(&down_one),
+            64
+        );
+
+        assert_eq!(
+            index.board_index_for_vector(&over_one_down_one),
+            65
+        );
+
+        assert_eq!(
+            index.board_index_for_vector(&end_of_world),
+            64 * 64 - 1
+        )
     }
 }
